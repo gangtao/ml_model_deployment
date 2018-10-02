daa90fbbd8149e090c4bc407bf0f327a47d4a657
Gang Tao <gtao@splunk.com>
2018-10-02 16:48:49 -0700

    Update seldon samples

--- seldon/pyspark/PySparkModel.py
+++ seldon/pyspark/PySparkModel.py
@@ -1,6 +1,8 @@
 from pyspark.ml import Pipeline
 from pyspark.ml import PipelineModel
 from pyspark import SparkConf, SparkContext
+from pyspark.sql.types import DoubleType, StructType, StructField
+from pyspark.sql import SparkSession
 
 import socket
 
@@ -11,6 +13,10 @@ conf = SparkConf().setAppName(
     'spark-deployment').setMaster('spark://spark-master:7077')
 sc = SparkContext(conf=conf)
 
+
+DEFAULT_HDFS_HOST = 'ad95fe885c37011e8aee806444a30499-1181034928.us-west-2.elb.amazonaws.com'
+
+
 class PySparkModel(object):
     """
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
@@ -20,19 +26,29 @@ class PySparkModel(object):
         """
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
-        self.server = "ad95fe885c37011e8aee806444a30499-1181034928.us-west-2.elb.amazonaws.com"
-        self.pipeline = None
+        self.hdfs = DEFAULT_HDFS_HOST
+        self.cSchema = StructType([StructField("sepal_length", DoubleType()),
+                                   StructField("sepal_width", DoubleType()),
+                                   StructField("petal_width", DoubleType()),
+                                   StructField("petal_length", DoubleType())])
         self.model = None
 
     def predict(self, X, features_names):
+        print(X)
+        print(type(X))
 
-        if self.pipeline is None:
-            self.pipeline = Pipeline.read().load(
-                "hdfs://{}:9000/tmp/classification-pipeline".format(self.server))
-            self.model = PipelineModel.read().load(
-                "hdfs://{}:9000/tmp/classification-model".format(self.server))
-        # Make predictions.
-        # TODO convert array to pandas data frame
-        data = X
-        predictions = self.model.transform(data)
-        return predictions
+        spark=SparkSession.builder.master("spark-master").getOrCreate()
+        data=spark.createDataFrame(X.tolist(), schema=self.cSchema)
+
+        print("Covert np array to list")
+        print(data)
+        print(type(data))
+        
+        if self.model is None:
+            # self.pipeline = Pipeline.read().load("hdfs://{}:9000/tmp/classification-pipeline".format(self.server))
+            self.model=PipelineModel.read().load(
+                "hdfs://{}:9000/tmp/classification-model".format(self.hdfs))
+
+        predictions=self.model.transform(data)
+        labels = predictions.select('predictedLabel').collect()
+        return [str(x.predictedLabel) for x in labels]
